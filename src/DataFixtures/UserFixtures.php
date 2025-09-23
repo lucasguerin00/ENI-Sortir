104c116b5350fca2844ce1c4ba4ed509debb6628
achaigneau2024 <chaigneau851@gmail.com>
2025-09-23 11:13:44 +0200

    Connexion utilisateur

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher){
+        $this->passwordHasher = $passwordHasher;
+    }
+    public function load(ObjectManager $manager): void
+    {
+        $userAdmin = new \App\Entity\Participant();
+        $userAdmin->setMail('chaigneau851@gmail.com');
+        $userAdmin->setNom('Chaigneau');
+        $userAdmin->setPrenom('Aimé');
+        $userAdmin->setIdSite(null);
+        $userAdmin->setTelephone("025103504");
+        $userAdmin->setIsAdministrateur(1);
+        $userAdmin->setIsActif(1);
+        $plaintextPassword = "1234";
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $userAdmin,
+            $plaintextPassword
+        );
+        $userAdmin->setPassword($hashedPassword);
+        $manager->persist($userAdmin);
+
+        $userCloe = new \App\Entity\Participant();
+        $userCloe->setMail('cloé@gmail.com');
+        $userCloe->setNom('Lemaréchal');
+        $userCloe->setPrenom('Cloé');
+        $userCloe->setIdSite(null);
+        $userCloe->setTelephone("025103504");
+        $userCloe->setIsAdministrateur(1);
+        $userCloe->setIsActif(1);
+        $plaintextPassword = "1234";
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $userCloe,
+            $plaintextPassword
+        );
+        $userCloe->setPassword($hashedPassword);
+        $manager->persist($userCloe);
+
+        $userLucas = new \App\Entity\Participant();
+        $userLucas->setMail('lucas@gmail.com');
+        $userLucas->setNom('Guérin');
+        $userLucas->setPrenom('Lucas');
+        $userLucas->setIdSite(null);
+        $userLucas->setTelephone("025103504");
+        $userLucas->setIsAdministrateur(1);
+        $userLucas->setIsActif(1);
+        $plaintextPassword = "1234";
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $userLucas,
+            $plaintextPassword
+        );
+        $userLucas->setPassword($hashedPassword);
+        $manager->persist($userLucas);
+
+        $manager->flush();
+    }
+}
