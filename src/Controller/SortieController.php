9b87b67c83a47f11f9b6bdff9bdd59e0552e22dd
lguerin31 <153713361+lucasguerin00@users.noreply.github.com>
2025-09-23 14:40:37 +0200

    CRUD CREATE SORTIE

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortieController extends AbstractController
+{
+    #[Route('/sorties', name: 'app_sortie_list')]
+    public function list(EntityManagerInterface $entityManager): Response
+    {
+        $sorties = $entityManager->getRepository(Sortie::class)->findAll();
+
+        return $this->render('sortie/list.html.twig', [
+            'sorties' => $sorties,
+        ]);
+    }
+
+    #[Route('/sortie/new', name: 'app_sortie_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $sortie = new Sortie();
+
+        $form = $this->createForm(SortieType::class, $sortie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Sortie créée avec succès !');
+
+            return $this->redirectToRoute('app_sortie_list');
+        }
+
+        return $this->render('sortie/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/SortieType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etat;
+use App\Entity\Lieu;
+use App\Entity\Participant;
+use App\Entity\Site;
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('duree', IntegerType::class)
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('nbInscriptionMax', IntegerType::class)
+            ->add('infosSortie', TextType::class)
+//            ->add('idSite', EntityType::class, [
+//                'class' => Site::class,
+//                'choice_label' => 'nom',
+//            ])
+//            ->add('idLieu', EntityType::class, [
+//                'class' => Lieu::class,
+//                'choice_label' => 'nom',
+//            ])
+            ->add('etat', EntityType::class, [
+                'class' => Etat::class,
+                'choice_label' => 'libelle'
+            ])
+            ->add('idOrganisateur', EntityType::class, [
+                'class' => Participant::class,
+                'choice_label' => 'id',
+            ])
+//            ->add('participants', EntityType::class, [
+//                'class' => Participant::class,
+//                'choice_label' => 'id',
+//                'multiple' => true,
+//            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
