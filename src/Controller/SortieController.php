ec7391a9f7ebe587212a0a728fd9f0151ee095d2
lguerin31 <153713361+lucasguerin00@users.noreply.github.com>
2025-09-25 11:42:44 +0200

    add sortie detail + inscription/desinscription

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -17,6 +17,8 @@ class SortieController extends AbstractController
     {
         return $this->redirectToRoute('app_sortie_list');
     }
+
+    // Affiche la liste des sorties
     #[Route('/sorties', name: 'app_sortie_list')]
     public function list(EntityManagerInterface $entityManager): Response
     {
@@ -27,6 +29,16 @@ class SortieController extends AbstractController
         ]);
     }
 
+    // Affiche une sortie
+    #[Route('/sortie/{id}', name: 'app_sortie_show')]
+    public function show(Sortie $sortie): Response
+    {
+        return $this->render('sortie/show.html.twig', [
+            'sortie' => $sortie,
+        ]);
+    }
+
+    // Affiche le formulaire de création des sorties
     #[Route('/sortie/new', name: 'app_sortie_new')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -57,6 +69,7 @@ class SortieController extends AbstractController
         ]);
     }
 
+    // Affiche le formulaire de modification des sorties
     #[Route('/sortie/{id}/edit', name: 'app_sortie_edit')]
     public function edit(Request $request, Sortie $sortie, EntityManagerInterface $entityManager): Response
     {
@@ -82,6 +95,7 @@ class SortieController extends AbstractController
         ]);
     }
 
+    // Annule une sortie
     #[Route('/sortie/{id}/delete', name: 'app_sortie_delete', methods: ['POST'])]
     public function delete(Request $request, Sortie $sortie, EntityManagerInterface $entityManager): Response
     {
@@ -101,6 +115,7 @@ class SortieController extends AbstractController
         return $this->redirectToRoute('app_sortie_list');
     }
 
+    // Archive une sortie
     #[Route('/sortie/{id}/archive', name: 'app_sortie_archive', methods: ['POST'])]
     public function archive(Request $request, Sortie $sortie, EntityManagerInterface $entityManager): Response
     {
@@ -119,5 +134,61 @@ class SortieController extends AbstractController
 
         return $this->redirectToRoute('app_sortie_list');
     }
+
+    //Gère l'inscription à une sortie
+    #[Route('/sortie/{id}/inscription', name: 'app_sortie_inscription')]
+    public function inscription(Sortie $sortie, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour vous inscrire.');
+        }
+
+        // Empêcher l’organisateur de s’inscrire à sa propre sortie
+        if ($sortie->getIdOrganisateur() === $user) {
+            $this->addFlash('warning', 'Vous êtes déjà l’organisateur de cette sortie.');
+            return $this->redirectToRoute('app_sortie_show', ['id' => $sortie->getId()]);
+        }
+
+        // Vérifier si déjà inscrit
+        if ($sortie->getParticipants()->contains($user)) {
+            $this->addFlash('warning', 'Vous êtes déjà inscrit à cette sortie.');
+            return $this->redirectToRoute('app_sortie_show', ['id' => $sortie->getId()]);
+        }
+
+        // Ajouter l’utilisateur comme participant
+        $sortie->addParticipant($user);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Inscription réussie !');
+
+        return $this->redirectToRoute('app_sortie_show', ['id' => $sortie->getId()]);
+    }
+
+    //Gère la désinscription à une sortie
+    #[Route('/sortie/{id}/desinscription', name: 'app_sortie_desinscription')]
+    public function desinscription(Sortie $sortie, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour vous désinscrire.');
+        }
+
+        if (!$sortie->getParticipants()->contains($user)) {
+            $this->addFlash('warning', 'Vous n’êtes pas inscrit à cette sortie.');
+            return $this->redirectToRoute('app_sortie_show', ['id' => $sortie->getId()]);
+        }
+
+        $sortie->removeParticipant($user);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Vous vous êtes désinscrit de la sortie.');
+
+        return $this->redirectToRoute('app_sortie_show', ['id' => $sortie->getId()]);
+    }
+
+
 
 }
